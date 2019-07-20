f914486ab01c5c93f7ee8ab2e7fa408664799097
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-20 23:11:45 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
   end
 
   post "/login" do
-    @user = User.find_by(username: params[:username])
+    @user = User.find_by_username(params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect to "/tweets"
